3a7d50a847cd4f2051a92a7686f7a7d5ff059557
Luis Mesajil <lhmesajil@gmail.com>
2024-09-25 00:39:49 -0500

    refactor(app): move App.tsx to app directory

---
+++ src/app/App.tsx
@@ -0,0 +1,14 @@
+import '@/app/App.css'
+import Footer from '@/popup/Footer/Footer'
+import { TextareaWithButton } from '@/popup/TextareaWithButton'
+
+function App() {
+  return (
+    <>
+      <TextareaWithButton />
+      <Footer />
+    </>
+  )
+}
+
+export default App
